c619f169caac0b2e42f5261afedab307a1bb543d
Nicolas Gotchac <ngotchac@gmail.com>
2018-11-26 17:54:09 +0100

    Don't sync all peers after each response

--- ethcore/sync/src/api.rs
+++ ethcore/sync/src/api.rs
@@ -379,9 +379,10 @@ impl SyncProvider for EthSync {
 }
 
 const PEERS_TIMER: TimerToken = 0;
-const SYNC_TIMER: TimerToken = 1;
-const COLLECT_BLOCKS_TIMER: TimerToken = 2;
-const TX_TIMER: TimerToken = 3;
+const MAINTAIN_SYNC_TIMER: TimerToken = 1;
+const CONTINUE_SYNC_TIMER: TimerToken = 2;
+const COLLECT_BLOCKS_TIMER: TimerToken = 3;
+const TX_TIMER: TimerToken = 4;
 
 struct SyncProtocolHandler {
 	/// Shared blockchain client.
@@ -398,7 +399,8 @@ impl NetworkProtocolHandler for SyncProtocolHandler {
 	fn initialize(&self, io: &NetworkContext) {
 		if io.subprotocol_name() != WARP_SYNC_PROTOCOL_ID {
 			io.register_timer(PEERS_TIMER, Duration::from_millis(700)).expect("Error registering peers timer");
-			io.register_timer(SYNC_TIMER, Duration::from_millis(1100)).expect("Error registering sync timer");
+			io.register_timer(MAINTAIN_SYNC_TIMER, Duration::from_millis(1100)).expect("Error registering sync timer");
+			io.register_timer(CONTINUE_SYNC_TIMER, Duration::from_millis(2_500)).expect("Error registering sync timer");
 			io.register_timer(COLLECT_BLOCKS_TIMER, Duration::from_millis(3000)).expect("Error registering collect blocks timer");
 			io.register_timer(TX_TIMER, Duration::from_millis(1300)).expect("Error registering transactions timer");
 		}
@@ -430,7 +432,8 @@ impl NetworkProtocolHandler for SyncProtocolHandler {
 		let mut io = NetSyncIo::new(io, &*self.chain, &*self.snapshot_service, &self.overlay);
 		match timer {
 			PEERS_TIMER => self.sync.write().maintain_peers(&mut io),
-			SYNC_TIMER => self.sync.write().maintain_sync(&mut io),
+			MAINTAIN_SYNC_TIMER => self.sync.write().maintain_sync(&mut io),
+			CONTINUE_SYNC_TIMER => self.sync.write().continue_sync(&mut io),
 			COLLECT_BLOCKS_TIMER => self.sync.write().collect_blocks(&mut io),
 			TX_TIMER => {
 				self.sync.write().propagate_new_transactions(&mut io);
